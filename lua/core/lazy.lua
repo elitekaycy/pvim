9be7c33d9be9e0001bac17111d9ef6f4a7298009
elitekaycy <dicksonanyaele1234@gmail.com>
2024-12-08 22:41:21 +0000

    format

--- lua/core/lazy.lua
+++ lua/core/lazy.lua
@@ -1,14 +1,14 @@
 local lazypath = "~/.local/share/pvim/lazy/lazy.nvim"
 
 if not vim.loop.fs_stat(vim.fn.expand(lazypath)) then
-	vim.fn.system({
-		"git",
-		"clone",
-		"--filter=blob:none",
-		"https://github.com/folke/lazy.nvim.git",
-		"--branch=stable",
-		vim.fn.expand(lazypath),
-	})
+    vim.fn.system({
+        "git",
+        "clone",
+        "--filter=blob:none",
+        "https://github.com/folke/lazy.nvim.git",
+        "--branch=stable",
+        vim.fn.expand(lazypath),
+    })
 end
 vim.opt.rtp:prepend(vim.fn.expand(lazypath))
 
--- lua/plugins/default/formatter.lua
+++ lua/plugins/default/formatter.lua
@@ -10,6 +10,7 @@ return {
                 typescript = { "prettier" },
                 javascriptreact = { "prettier" },
                 typescriptreact = { "prettier" },
+                ftl = { 'prettier' },
                 java = {
                     'google-java-format',
                 },
