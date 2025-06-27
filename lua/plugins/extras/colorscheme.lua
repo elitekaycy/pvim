6fc4fd708549f80a1769e2944920ca2591b662ab
samuel-nana-baffour.asare@external.telekom.de <samuel-nana-baffour.asare@external.telekom.de>
2025-06-27 11:13:24 +0000

    color scheme change pvim

--- lua/plugins/extras/colorscheme.lua
+++ lua/plugins/extras/colorscheme.lua
@@ -1,7 +1,21 @@
+-- return {
+-- 	"rebelot/kanagawa.nvim",
+-- 	config = function()
+-- 		require("kanagawa").setup()
+-- 		vim.cmd("colorscheme kanagawa")
+-- 	end,
+-- }
+--
 return {
-	"rebelot/kanagawa.nvim",
-	config = function()
-		require("kanagawa").setup()
-		vim.cmd("colorscheme kanagawa")
-	end,
+    "rebelot/kanagawa.nvim",
+    config = function()
+        require("kanagawa").setup({
+            transparent = true, -- this disables the background color
+        })
+        vim.cmd("colorscheme kanagawa")
+
+        -- Optional: make Normal and Float windows more see-through
+        vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
+        vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
+    end,
 }
